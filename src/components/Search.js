848b474c954f85fff1f98ca391221b7b46768816
Anton Serdyuchenko <anton415@gmail.com>
2018-12-22 11:15:40 +0300

    Add all search components.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,17 @@
 import React, {Component} from 'react';
+import './Search.css';
 
 class Search extends Component {
   render() {
     return (
-      <div className="Search">Search</div>
+      <div className="Search">
+        <div className="Title">Bart location</div>
+        <div className="Search-field">
+          <div className="Search-input">button</div>
+          <div className="Filter-button">filter</div>
+        </div>
+        <div className="List-of-location">List of location</div>
+      </div>
     )
   }
 }
