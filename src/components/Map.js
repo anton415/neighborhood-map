c5ba3eac56680ea3a8ae17078062e9868964c744
Anton Serdyuchenko <anton415@gmail.com>
2018-12-28 06:09:51 +0300

    Add five marks.

--- src/components/Map.js
+++ src/components/Map.js
@@ -45,7 +45,7 @@ class MapContainer extends Component {
       <div className="Map">
         <Map
           google={this.props.google}
-          zoom={14}
+          zoom={13}
           style={mapStyles}
           initialCenter={{
            lat: 55.86430000000001,
@@ -55,6 +55,68 @@ class MapContainer extends Component {
             <Marker
               onClick={this.onMarkerClick}
               name={'My home.'}
+            />
+            <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}
+              onClose={this.onClose}
+            >
+              <div>
+                <h4>{this.state.selectedPlace.name}</h4>
+              </div>
+            </InfoWindow>
+
+            <Marker
+              onClick={this.onMarkerClick}
+              name={'My school.'}
+              position={{lat: 55.868346, lng: 37.099723}}
+            />
+            <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}
+              onClose={this.onClose}
+            >
+              <div>
+                <h4>{this.state.selectedPlace.name}</h4>
+              </div>
+            </InfoWindow>
+
+            <Marker
+              onClick={this.onMarkerClick}
+              name={'Sport complecs.'}
+              position={{lat: 55.859081, lng: 37.12836}}
+            />
+            <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}
+              onClose={this.onClose}
+            >
+              <div>
+                <h4>{this.state.selectedPlace.name}</h4>
+              </div>
+            </InfoWindow>
+
+            <Marker
+              onClick={this.onMarkerClick}
+              name={'City Hospital.'}
+              position={{lat: 55.858924, lng: 37.12394}}
+            />
+            <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}
+              onClose={this.onClose}
+            >
+              <div>
+                <h4>{this.state.selectedPlace.name}</h4>
+              </div>
+            </InfoWindow>
+
+            <Marker
+              onClick={this.onMarkerClick}
+              name={'Bakery plant. One of the attractions of my city. ' +
+               'Everyone loves the city trying to buy bread in this place, ' +
+               'because it is very tasty.'}
+              position={{lat: 55.865475, lng: 37.122159}}
             />
             <InfoWindow
               marker={this.state.activeMarker}
